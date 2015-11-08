22b3d85d1427d0f6e02ebec14faefde7da1353b4
Mladen Mijatov <meaneye.rcf@gmail.com>
2015-11-08 17:31:27 +0100

    Initial import.

---
+++ bashrc
@@ -0,0 +1,4 @@
+
+# aliases
+alias caracal_update='vagrant ssh -c "cd /var/www;sudo git pull origin"'
+alias cpass='PASSWORD_STORE_DIR=~/.password-store/Way2CU pass'
---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# link current directory with home directory
+if [ ! -d ~/.way2cu ]
+then
+	echo "Linking directory to home..."
+	ln -s `pwd` ~/.way2cu
+fi
+
+# clone credentials directory
+if [ -d ~/.password-store ] && [ ! -d ~/.password-store/Way2CU ]
+then
+	echo "Cloning credentials repository..."
+	git clone git@github.com:Way2CU/Credentials.git ~/.password-store/Way2CU
+fi
+
+# load company scripts
+if ! grep -Fxq "source ~/.way2cu/bash.sh" ~/.bashrc
+then
+	echo "Configuring bash resource file..."
+	echo "source ~/.way2cu/bashrc" >> ~/.bashrc
+fi
